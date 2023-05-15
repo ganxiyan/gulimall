14989a8a81ea32266476c4c99380487b17c34eb6
root <ganxiyan@cug.edu.cn>
2023-05-15 13:26:12 +0800

    项目创建

---
+++ gulimall-product/src/test/java/com/gxy/gulimall/lproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxy.gulimall.lproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
